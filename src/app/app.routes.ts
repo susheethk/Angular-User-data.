188fd9ef029c322e5478efd5153a25a7c57a7201
susheethk <susheethk@users.noreply.github.com>
2024-08-26 21:19:14 +0530

    added new page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,6 @@ import { PagetwoComponent } from './pagetwo/pagetwo.component';
 export const routes: Routes = [
   { path: 'pageone', component: PageoneComponent },
   { path: 'pagetwo', component: PagetwoComponent },
-  { path: '', redirectTo: '/pageone', pathMatch: 'full' }, // Redirect to 'pageone' by default
-  { path: '**', redirectTo: '/pageone' } // Redirect any unknown paths to 'pageone'
+  { path: '', redirectTo: '/pageone', pathMatch: 'full' }
+ 
 ];
---
+++ src/app/pagethree/pagethree.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagethree',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagethree.component.html',
+  styleUrl: './pagethree.component.css'
+})
+export class PagethreeComponent {
+
+}
